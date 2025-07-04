c296c2a5310803c55de3f04aad36737128c91dd8
hojat72elect <hojat72elect@gmail.com>
2025-07-04 03:16:07 -0400

    LifecycleListener.java -> kt

---
+++ libgdx/gdx/src/com/badlogic/gdx/LifecycleListener.kt
@@ -0,0 +1,30 @@
+package com.badlogic.gdx
+
+
+/**
+ * A LifecycleListener can be added to an [Application] via [Application.addLifecycleListener]. It
+ * will receive notification of pause, resume and dispose events. This is mainly meant to be used by extensions that need to
+ * manage resources based on the life-cycle. Normal, application level development should rely on the [ApplicationListener]
+ * interface.
+ *
+ *
+ *
+ * The methods will be invoked on the rendering thread. The methods will be executed before the [ApplicationListener]
+ * methods are executed.
+ */
+interface LifecycleListener {
+    /**
+     * Called when the [Application] is about to pause
+     */
+    fun pause()
+
+    /**
+     * Called when the Application is about to be resumed
+     */
+    fun resume()
+
+    /**
+     * Called when the [Application] is about to be disposed
+     */
+    fun dispose()
+}
