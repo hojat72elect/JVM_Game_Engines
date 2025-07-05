7ab043bdc636d8370af8ffb520e85fc0d56fc307
hojat72elect <hojat72elect@gmail.com>
2025-07-05 17:41:31 -0400

    ApplicationListener.java -> kt

---
+++ libgdx/gdx/src/com/badlogic/gdx/ApplicationListener.kt
@@ -0,0 +1,44 @@
+package com.badlogic.gdx
+
+/**
+ * An [ApplicationListener]is called when the [Application] is created, resumed, rendering, paused or destroyed.
+ * All methods are called in a thread that has the OpenGL context current. You can thus safely create and manipulate graphics resources.
+ *
+ * The [ApplicationListener] interface follows the standard Android activity life-cycle and is emulated on the desktop accordingly (just consider this as a lifecycle listener).
+ */
+interface ApplicationListener {
+
+    /**
+     * Called when the [Application] is first created.
+     */
+    fun create()
+
+    /**
+     * Called when the [Application] is resized. This can happen at any point during a non-paused state but will never happen before a call to [create].
+     *
+     * @param width  the new width in pixels.
+     * @param height the new height in pixels.
+     */
+    fun resize(width: Int, height: Int)
+
+    /**
+     * Called when the [Application] should render (or re-render) itself.
+     */
+    fun render()
+
+    /**
+     * Called when the [Application] is paused, usually when it's not active or visible on-screen. An Application is also
+     * paused before it is destroyed.
+     */
+    fun pause()
+
+    /**
+     * Called when the [Application] is resumed from a paused state, usually when it regains focus on screen.
+     */
+    fun resume()
+
+    /**
+     * Called when the [Application] is destroyed. Preceded by a call to [pause].
+     */
+    fun dispose()
+}
